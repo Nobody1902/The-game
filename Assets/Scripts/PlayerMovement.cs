1eac2605b95e32d9e79172d298d6b89ba4b5161a
Nobody1902 <vitan.s.jaklic@gmail.com>
2023-12-09 20:00:49 +0100

    Added PlayerMovement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -10,13 +9,38 @@ public class PlayerMovement : MonoBehaviour
     public const float G = 9.81f;
     private Rigidbody2D rb;
 
+    [Header("Settings")]
+    public float mass = 10;
+    public float movementSpeed = 100;
+    public bool grounded = false;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.mass = mass;
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(G * mass * Vector2.down);
     }
     private void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.position += movementSpeed * Time.deltaTime * Vector2.left;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.position += movementSpeed * Time.deltaTime * Vector2.right;
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        grounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
 }
